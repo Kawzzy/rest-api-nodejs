237389969506e78420b51e648949abdfc21d3bab
Augusto Kalahary <AuuKalaharyKW@gmail.com>
2023-08-29 20:54:25 -0300

    Testing the GET route to list all transactions.

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,7 +1,7 @@
 import request from 'supertest'
 
 import { app } from '../src/app'
-import { afterAll, beforeAll, describe, test } from 'vitest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
 describe('Transactions routes', () => {
   beforeAll(async () => {
@@ -13,7 +13,7 @@ describe('Transactions routes', () => {
   })
 
   // to create a test, we can either use test() or it()
-  test('User can create a new transaction', async () => {
+  it('should create a new transaction', async () => {
     await request(app.server)
       .post('/transactions')
       .send({
@@ -22,5 +22,29 @@ describe('Transactions routes', () => {
         type: 'credit',
       })
       .expect(201)
+  })
+
+  it('should list all the transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'Transaction test',
+        amount: 270,
+        type: 'credit',
+      })
+
+    const cookie = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookie)
+      .expect(200)
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'Transaction test',
+        amount: 270,
+      }),
+    ])
   })
 })
